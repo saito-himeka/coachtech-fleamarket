a7862df7fea0f3f715a318f65a22610096862e0c
saito himeka <pafu1217@gmail.com>
2026-02-06 18:22:03 +0900

    シーディングファイルの修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -6,15 +6,11 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Item;
 use App\Models\Condition;
+use App\Models\Category; // 追加
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         // 1. テスト用ユーザーの作成
@@ -25,115 +21,102 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // 2. コンディションの作成
-        // 仕様書の文言に合わせて作成します
-        $conditions = [
-            '良好',
-            '目立った傷や汚れなし',
-            'やや傷や汚れあり',
-            '状態が悪い'
-        ];
-
+        $conditions = ['良好', '目立った傷や汚れなし', 'やや傷や汚れあり', '状態が悪い'];
         $conditionIds = [];
         foreach ($conditions as $name) {
             $c = Condition::create(['name' => $name]);
             $conditionIds[$name] = $c->id;
         }
 
-        // 3. 商品データの作成 
-        $items = [
+        // 3. カテゴリの作成
+        // 主要なカテゴリをあらかじめ作成しておきます
+        $categoryNames = ['ファッション', '家電', 'インテリア', 'レディース', 'メンズ', 'コスメ', 'キッチン', 'ハンドメイド', 'アクセサリー', 'おもちゃ', 'ベビー・キッズ'];
+        $categories = [];
+        foreach ($categoryNames as $name) {
+            $categories[$name] = Category::create(['name' => $name]);
+        }
+
+        // 4. 商品データの作成
+        $itemsData = [
             [
-                'name' => '腕時計',
-                'price' => 15000,
-                'brand' => 'Rolax',
+                'name' => '腕時計', 'price' => 15000, 'brand' => 'Rolax', 'condition' => '良好',
                 'description' => 'スタイリッシュなデザインのメンズ腕時計',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
-                'condition' => '良好'
+                'category_list' => ['ファッション', 'メンズ', 'アクセサリー']
             ],
             [
-                'name' => 'HDD',
-                'price' => 5000,
-                'brand' => '西芝',
+                'name' => 'HDD', 'price' => 5000, 'brand' => '西芝', 'condition' => '目立った傷や汚れなし',
                 'description' => '高速で信頼性の高いハードディスク',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
-                'condition' => '目立った傷や汚れなし'
+                'category_list' => ['家電']
             ],
             [
-                'name' => '玉ねぎ3束',
-                'price' => 300,
-                'brand' => 'なし',
+                'name' => '玉ねぎ3束', 'price' => 300, 'brand' => 'なし', 'condition' => 'やや傷や汚れあり',
                 'description' => '新鮮な玉ねぎ3束のセット',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
-                'condition' => 'やや傷や汚れあり'
+                'category_list' => ['キッチン']
             ],
             [
-                'name' => '革靴',
-                'price' => 4000,
-                'brand' => null,
+                'name' => '革靴', 'price' => 4000, 'brand' => null, 'condition' => '状態が悪い',
                 'description' => 'クラシックなデザインの革靴',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
-                'condition' => '状態が悪い'
+                'category_list' => ['ファッション', 'メンズ']
             ],
             [
-                'name' => 'ノートPC',
-                'price' => 45000,
-                'brand' => null,
+                'name' => 'ノートPC', 'price' => 45000, 'brand' => null, 'condition' => '良好',
                 'description' => '高性能なノートパソコン',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
-                'condition' => '良好'
+                'category_list' => ['家電']
             ],
             [
-                'name' => 'マイク',
-                'price' => 8000,
-                'brand' => 'なし',
+                'name' => 'マイク', 'price' => 8000, 'brand' => 'なし', 'condition' => '目立った傷や汚れなし',
                 'description' => '高音質のレコーディング用マイク',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
-                'condition' => '目立った傷や汚れなし'
+                'category_list' => ['家電']
             ],
             [
-                'name' => 'ショルダーバッグ',
-                'price' => 3500,
-                'brand' => null,
+                'name' => 'ショルダーバッグ', 'price' => 3500, 'brand' => null, 'condition' => 'やや傷や汚れあり',
                 'description' => 'おしゃれなショルダーバッグ',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
-                'condition' => 'やや傷や汚れあり'
+                'category_list' => ['ファッション', 'レディース']
             ],
             [
-                'name' => 'タンブラー',
-                'price' => 500,
-                'brand' => 'なし',
+                'name' => 'タンブラー', 'price' => 500, 'brand' => 'なし', 'condition' => '状態が悪い',
                 'description' => '使いやすいタンブラー',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
-                'condition' => '状態が悪い'
+                'category_list' => ['キッチン', 'インテリア']
             ],
             [
-                'name' => 'コーヒーミル',
-                'price' => 4000,
-                'brand' => 'Starbacks',
+                'name' => 'コーヒーミル', 'price' => 4000, 'brand' => 'Starbacks', 'condition' => '良好',
                 'description' => '手動のコーヒーミル',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
-                'condition' => '良好'
+                'category_list' => ['キッチン', '家電']
             ],
             [
-                'name' => 'メイクセット',
-                'price' => 2500,
-                'brand' => null,
+                'name' => 'メイクセット', 'price' => 2500, 'brand' => null, 'condition' => '目立った傷や汚れなし',
                 'description' => '便利なメイクアップセット',
                 'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
-                'condition' => '目立った傷や汚れなし'
+                'category_list' => ['コスメ']
             ],
         ];
 
-        foreach ($items as $data) {
-            Item::create([
+        foreach ($itemsData as $data) {
+            $item = Item::create([
                 'user_id' => $user->id,
                 'condition_id' => $conditionIds[$data['condition']],
                 'name' => $data['name'],
                 'price' => $data['price'],
                 'brand' => $data['brand'],
                 'description' => $data['description'],
-                // ER図の image_paths カラムに合わせて配列で格納
                 'image_paths' => [$data['image_url']],
             ]);
+
+            // カテゴリの紐付け（中間テーブルへの挿入）
+            foreach ($data['category_list'] as $catName) {
+                if (isset($categories[$catName])) {
+                    $item->categories()->attach($categories[$catName]->id);
+                }
+            }
         }
     }
 }
